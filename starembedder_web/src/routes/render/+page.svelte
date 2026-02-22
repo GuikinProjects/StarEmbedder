484f8924b623ddb0fb83749f77772d0cad2c8187
GuikiPT <foxzillapt17@gmail.com>
2026-02-22 20:51:38 +0000

    [REMOVE] "Today At" string since it's from old discord versions and is not the reality on present

--- starembedder_web/src/routes/render/+page.svelte
+++ starembedder_web/src/routes/render/+page.svelte
@@ -37,7 +37,7 @@
 			hour12: true
 		});
 
-		if (date.toDateString() === now.toDateString()) return `Today at ${timeStr}`;
+		if (date.toDateString() === now.toDateString()) return `${timeStr}`;
 		if (date.toDateString() === yesterday.toDateString()) return `Yesterday at ${timeStr}`;
 		return (
 			date.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }) +
