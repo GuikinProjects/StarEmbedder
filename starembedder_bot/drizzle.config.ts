b8a9f33608e432128cfe311a28a0d3c9d3597a90
GuikiPT <foxzillapt17@gmail.com>
2026-02-22 19:26:26 +0000

    [FIX] update database path in drizzle configuration

--- starembedder_bot/drizzle.config.ts
+++ starembedder_bot/drizzle.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
-import { join } from 'node:path';
 
-const dbPath = process.env.DATABASE_PATH ?? join(process.cwd(), 'src', 'lib', 'db', 'data', 'skullboard.db');
 
 export default defineConfig({
 	dialect: 'sqlite',
 	schema: './src/lib/db/schema.ts',
 	out: './src/lib/db/migrations',
 	dbCredentials: {
-		url: dbPath
+		url: 'file:./src/lib/db/starembedder.db'
 	}
 });
